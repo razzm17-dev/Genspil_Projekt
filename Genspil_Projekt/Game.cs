3eb57ae420615987f2e4a7f4d77e15110fb746ab
razz <razzm17@gmail.com>
2026-04-07 10:11:04 +0200

    Add project files.

---
+++ Genspil_Projekt/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Genspil_Projekt
+{
+    public class Game
+    {
+        private string _name;
+        private string _genre;
+        private int _playerCount;
+
+        private List
+
+    }
+}
---
+++ Genspil_Projekt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Genspil_Projekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
